987c331e708a317cf6857552b60b74d8bd74ab65
Alexander <alexander.assink@gmail.com>
2020-02-22 15:30:19 +0100

    Page formatting
    No changes, just some formatting

--- frontend/src/pages/WalletInformation.vue
+++ frontend/src/pages/WalletInformation.vue
@@ -1,18 +1,12 @@
 <template>
-    <div id="app" class="row">
-      <div class="col-xl-6 col-lg-7 col-md-6">
-        
-        <user-card>
-
-        </user-card>
-        
-      </div>
-      <div class="col-xl-6 col-lg-7 col-md-6">
-        <edit-profile-form>
-          
-        </edit-profile-form>
-      </div>
+  <div id="app" class="row">
+    <div class="col-xl-6 col-lg-7 col-md-6">
+      <user-card></user-card>
     </div>
+    <div class="col-xl-6 col-lg-7 col-md-6">
+      <edit-profile-form></edit-profile-form>
+    </div>
+  </div>
 </template>
 <script>
 import EditProfileForm from "./UserProfile/EditProfileForm.vue";

3f6cca21092a919bca8fe4afb07b15fb654d1d7b
grvlle <gralle89@gmail.com>
2020-03-18 06:34:35 +0100

    Updated NPM dependencies and added new testnet information

--- app.go
+++ app.go
@@ -150,13 +150,13 @@ func (a *WalletApplication) initDirectoryStructure() error {
 
 // initMainnetConnection populates the WalletApplication struct with mainnet data
 func (a *WalletApplication) initMainnetConnection() {
-	a.Network.URL = "http://cl-lb-alb-test-1471406049.us-west-1.elb.amazonaws.com:9000" // Temp
+	a.Network.URL = "http://cl-lb-alb-testnet-118182741.us-west-1.elb.amazonaws.com:9000" // Temp
 
 	a.Network.Handles.Send = "/send"
 	a.Network.Handles.Transaction = "/transaction"
 	a.Network.Handles.Balance = "/balance/"
 
-	a.Network.BlockExplorer.URL = "https://9vxbd3xkg6.execute-api.us-west-1.amazonaws.com/cl-block-explorer-test"
+	a.Network.BlockExplorer.URL = "https://3pii1fjixi.execute-api.us-west-1.amazonaws.com/cl-block-explorer-testnet"
 	a.Network.BlockExplorer.Handles.Transactions = "/transactions/"
 	a.Network.BlockExplorer.Handles.Checkpoints = "/checkpoints/"
 	a.Network.BlockExplorer.Handles.Snapshots = "/snapshots/"

6958a0f0c0e212146c96f380e59ea5b6424a4ae8
Martin Granström <gralle89@gmail.com>
2020-05-28 12:29:16 +0200

    Update backend/cmd/update/main.go
    Co-authored-by: Joakim Söderberg <netrounds-joakim@users.noreply.github.com>

--- backend/cmd/update/main.go
+++ backend/cmd/update/main.go
@@ -18,7 +18,7 @@ import (
 func init() {
 
 	// initialize update.log file and set log output to file
-	file, err := os.OpenFile(getDefaultDagFolderPath()+"/update.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile(path.Join(getDefaultDagFolderPath(), "update.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
 		log.SetOutput(file)
 	} else {

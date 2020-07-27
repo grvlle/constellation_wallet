171e6c86c1bf2a48cae62a26c37d7763f94bba27
digitaltwinnn <45341252+digitaltwinnn@users.noreply.github.com>
2020-07-27 17:55:56 +0200

    Changed according to the go conventions

--- backend/models/contact.go
+++ backend/models/contact.go
@@ -2,7 +2,7 @@ package models
 
 // Contact holds the DAG address and other contact details
 type Contact struct {
-	ID          uint   `json:"id" gorm:"AUTO_INCREMENT"`
+	ID          uint   `json:"id" gorm:"auto_increment"`
 	Alias       string `json:"alias"`
 	Address     string `json:"address"`
 	Name        string `json:"name"`

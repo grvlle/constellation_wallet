77f12f03e7a005576587f4026420ac5220cd5823
Frank Fox <ffox77@yahoo.com>
2020-10-22 14:40:30 -0700

    fix not used build error

--- backend/utils.go
+++ backend/utils.go
@@ -79,8 +79,8 @@ func (a *WalletApplication) detectJavaPath() {
 
 		err := cmd.Run()
 		if err != nil {
-			//errFormatted := fmt.Sprint(err) + ": " + stderr.String()
-			//a.log.Errorf(errFormatted)
+			errFormatted := fmt.Sprint(err) + ": " + stderr.String()
+			a.log.Errorf(errFormatted)
 			//a.LoginError("Unable to find Java Installation")
 			//a.paths.Java = "No valid path detected"
 			a.paths.Java = "javaw.exe"

5ce64163770cb7d1b4b19ca31d9c2e32919d428f
Frank Fox <ffox77@yahoo.com>
2020-11-03 07:23:46 -0800

    update ui version

--- frontend/src/store/modules/app.js
+++ frontend/src/store/modules/app.js
@@ -1,7 +1,7 @@
 const getDefaultState = () => {
   return {
     version: "v2.13.9",
-    uiVersion: "v2.0.1",
+    uiVersion: "v2.0.2",
     isLoggedIn: false,
     errorMessage: "",
     warningMessage: "",
